54d0d7b960ba841eb21ce4bfa22f25f86698b4d3
Zepse Wolf <douglasmeows@gmail.com>
2019-01-24 10:32:03 +0800

    c

--- routes/users.js
+++ routes/users.js
@@ -49,7 +49,7 @@ router.get('/newDatas', function(req, res) {
     });
 });
 router.get('/getCategory', (req,res)=>{
-    discovery.query({ environment_id: `17bc5cf7-1be3-4f8e-a06f-9ddec7317aec`, collection_id: `d47a72a6-07c6-4aad-aa36-4944659d6589`,similar:true,similar_document_ids:"992d725c-e547-420e-8b5d-ef376dedfe54"},function(e,data){
+    discovery.query({ environment_id: `17bc5cf7-1be3-4f8e-a06f-9ddec7317aec`, collection_id: `d47a72a6-07c6-4aad-aa36-4944659d6589`,_id:"992d725c-e547-420e-8b5d-ef376dedfe54"},function(e,data){
         if (e)
         res.send(e);
         else{
