69214d7bd4aa95ec002754f86ab5e66319de6d18
Zepse Wolf <douglasmeows@gmail.com>
2019-02-15 16:43:00 +0800

    c

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 var mongoose =require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://admin1234:meow1234@ds046677.mlab.com:46677/intermatch',{useNewUrlParser: true});
-mongoose.connect('mongodb+srv://L33903:NYPfyp339@cluster0-uv7is.mongodb.net/test?retryWrites=true',{useNewUrlParser: true});
+mongoose.connect('mongodb://admin1234:meow1234@ds046677.mlab.com:46677/intermatch',{useNewUrlParser: true});
 //passport
 var passport = require('passport');
 var session = require('express-session');
